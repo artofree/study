2891003b4e28cd9f8c6013bf8b9fdcad2a6f9c3f
artofree <artofree@me.com>
2015-04-19 20:59:29 +0800

    1

---
+++ pythons/panoramio/parser.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#抽取图片统计信息
+#photoid,userid,lt,ltc,ln,lnc,looks,likes,favorites,comments,photo_title,taken_time,tags
+#其中tags试图与标准tags对应，梳理
+
+import os, urllib2, threading, codecs, time
+from pyquery import PyQuery as pq
+from lxml import etree
+
+
+for  range(1,11)
+
