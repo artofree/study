7b6468c4eed61e988e29cc9c3b20bfc58f19d0a9
guo <artofree@me.com>
2016-05-07 15:56:04 +0800

    1

--- ml/tensorflow/my_mnist.py
+++ ml/tensorflow/my_mnist.py
@@ -19,6 +19,8 @@ sess.run(init)
 for i in range(1000):
   batch_xs, batch_ys = mnist.train.next_batch(100)
   sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+  # sess.run([train_step ,cross_entropy], feed_dict={x: batch_xs, y_: batch_ys})
+  # print(cross_entropy)
 
 correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
