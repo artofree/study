b2f581664f68260b720bda6e4139d763e1a51add
artofree <artofree@me.com>
2016-06-06 15:27:00 +0800

    1

--- hupai/web/51hupai.py
+++ hupai/web/51hupai.py
@@ -2,11 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from sikuli import *
 
-driver =webdriver.Chrome()
-driver.get("http://www.google.com")
+driver =webdriver.Ie()
+driver.get("http://test.alltobid.com/")
 
-elem = driver.find_element_by_id("lst-ib")
-elem.send_keys("123")
-elem.send_keys(Keys.RETURN)
+
 assert "No results found."
-driver.close()
