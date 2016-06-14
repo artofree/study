79119a6f4d4848bb3f2f70f60f59baf4d0b39662
guo <artofree@me.com>
2016-06-14 13:57:50 +0800

    1

--- hupai/opencv/模板匹配.py
+++ hupai/opencv/模板匹配.py
@@ -2,12 +2,18 @@ __author__ = 'guo'
 # -*- coding: utf-8 -*-
 
 import cv2
-from PIL import ImageGrab
+from PIL import ImageGrab ,Image
 import numpy as np
 from matplotlib import pyplot as plt
 
 img =ImageGrab.grab()
 tmp =ImageGrab.grab((100 ,100 ,400 ,400))
+img =np.array(img, dtype=np.uint8)
+tmp =np.array(tmp, dtype=np.uint8)
+w, h = 300 ,300
+method = eval('cv2.TM_CCOEFF')
+res = cv2.matchTemplate(img,tmp,method)
+min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
 
 cv2.imshow('image', img)
 k = cv2.waitKey(0) &0xFF
--- hupai/thePrj/theLib/damatu.py
+++ hupai/thePrj/theLib/damatu.py
@@ -1,3 +1,6 @@
+
+#python版本3.4
+
 import hashlib
 import http.client
 import urllib.request
@@ -8,39 +11,39 @@ import base64
 def md5str(str): #md5加密字符串
 		m=hashlib.md5(str.encode(encoding = "utf-8"))
 		return m.hexdigest()
-
+		
 def md5(byte): #md5加密byte
 		return hashlib.md5(byte).hexdigest()
-
+		
 class DamatuApi():
-
+	
 	ID = '40838'
 	KEY = 'ca9507e17e8d5ddf7c57cd18d8d33010'
 	HOST = 'http://api.dama2.com:7766/app/'
-
-
+	
+	
 	def __init__(self,username,password):
 		self.username=username
 		self.password=password
-
+		
 	def getSign(self,param=b''):
 		return (md5(bytes(self.KEY, encoding = "utf8") + bytes(self.username, encoding = "utf8") + param))[:8]
-
+		
 	def getPwd(self):
 		return md5str(self.KEY +md5str(md5str(self.username) + md5str(self.password)))
-
+		
 	def post(self,path,params={}):
 		data = urllib.parse.urlencode(params).encode('utf-8')
 		url = self.HOST + path
 		response = urllib.request.Request(url,data)
 		return urllib.request.urlopen(response).read()
-
+	
 	#查询余额 return 是正数为余额 如果为负数 则为错误码
 	def getBalance(self):
 		data={'appID':self.ID,
 			'user':self.username,
-			'pwd':dmt.getPwd(),
-			'sign':dmt.getSign()
+			'pwd':self.getPwd(),
+			'sign':self.getSign()
 		}
 		res = self.post('d2Balance',data)
 		res = str(res, encoding = "utf-8")
@@ -49,7 +52,7 @@ class DamatuApi():
 			return jres['balance']
 		else:
 			return jres['ret']
-
+    
 	#上传验证码 参数filePath 验证码图片路径 如d:/1.jpg type是类型，查看http://wiki.dama2.com/index.php?n=ApiDoc.Pricedesc  return 是答案为成功 如果为负数 则为错误码
 	def decode(self,filePath,type):
 		f=open(filePath,'rb')
@@ -58,11 +61,11 @@ class DamatuApi():
 		f.close()
 		data={'appID':self.ID,
 			'user':self.username,
-			'pwd':dmt.getPwd(),
+			'pwd':self.getPwd(),
 			'type':type,
 			'fileDataBase64':filedata,
-			'sign':dmt.getSign(fdata)
-		}
+			'sign':self.getSign(fdata)
+		}		
 		res = self.post('d2File',data)
 		res = str(res, encoding = "utf-8")
 		jres = json.loads(res)
@@ -71,15 +74,15 @@ class DamatuApi():
 			return(jres['result'])
 		else:
 			return jres['ret']
-
+		
 	#url地址打码 参数 url地址  type是类型(类型查看http://wiki.dama2.com/index.php?n=ApiDoc.Pricedesc) return 是答案为成功 如果为负数 则为错误码
 	def decodeUrl(self,url,type):
 		data={'appID':self.ID,
 			'user':self.username,
-			'pwd':dmt.getPwd(),
+			'pwd':self.getPwd(),
 			'type':type,
 			'url':urllib.parse.quote(url),
-			'sign':dmt.getSign(url.encode(encoding = "utf-8"))
+			'sign':self.getSign(url.encode(encoding = "utf-8"))
 		}
 		res = self.post('d2Url',data)
 		res = str(res, encoding = "utf-8")
@@ -89,7 +92,7 @@ class DamatuApi():
 			return(jres['result'])
 		else:
 			return jres['ret']
-
+	
 	#报错 参数id(string类型)由上传打码函数的结果获得 return 0为成功 其他见错误码
 	def reportError(self,id):
 		#f=open('0349.bmp','rb')
@@ -97,10 +100,10 @@ class DamatuApi():
 		#print(md5(fdata))
 		data={'appID':self.ID,
 			'user':self.username,
-			'pwd':dmt.getPwd(),
+			'pwd':self.getPwd(),
 			'id':id,
-			'sign':dmt.getSign(id.encode(encoding = "utf-8"))
-		}
+			'sign':self.getSign(id.encode(encoding = "utf-8"))
+		}	
 		res = self.post('d2ReportError',data)
 		res = str(res, encoding = "utf-8")
 		jres = json.loads(res)
